46e9487b5a450da5a7db4e5b2af9f52559a67de2
Nick Schmidthüs <nick.schmidthues@ibs-banking.com>
2021-09-14 11:08:42 +0200

    Element attacks

--- Element.java
+++ Element.java
@@ -2,15 +2,23 @@ public class Element {
     public String name[] = {"Water", "Fire", "Earth", "Air"};
 
     public void Tsunami (){
+        int ap = 25;
+        System.out.println("A giant wave rolls over the enemy and destroys everything in the way!");
 
     }
     public void Ignition () {
+        int ap = 25;
+        System.out.println("Fire strikes on the enemy!");
 
     }
     public void Tornado () {
+        int ap = 25;
+        System.out.println("Heavy winds sling all sorts of debris towards the enemy!");
 
     }
     public void Earthquake () {
+        int ap = 25;
+        System.out.println("The earth is shaking! Cracks in the ground and debris falling down on the target!");
 
     }
 }
