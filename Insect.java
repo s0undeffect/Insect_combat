cd0c3fc81dd5cdd070b4495999c3a20f6ea96c85
s0undeffect <87813578+s0undeffect@users.noreply.github.com>
2021-09-14 08:07:48 +0200

    Add files via upload

---
+++ Insect.java
@@ -0,0 +1,13 @@
+public class Insect {
+    public int hp; //HealthPoints
+    public int ap; //AttackPoints
+    public int ad; //AttackDamage
+
+    public void normalAttack(){
+
+    }
+
+    public void specialAttack(){
+
+    }
+}
