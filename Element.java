158ef7777eff05608d6052598d90ceb67454f455
Anel Meirzhan <anel.meirzhan@ibs-banking.com>
2021-09-14 08:24:51 +0200

    Element

---
+++ Element.java
@@ -0,0 +1,16 @@
+public class Element {
+    public String name[] = {"Water", "Fire", "Earth", "Air"};
+
+    public void Tsunami (){
+
+    }
+    public void Ignition () {
+
+    }
+    public void Tornado () {
+
+    }
+    public void Earthquake () {
+
+    }
+}
